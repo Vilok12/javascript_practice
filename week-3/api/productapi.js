bcf98b4a196cf9101961115c278e14b2a4eaf2d6
Vilok12 <151763264+Vilok12@users.noreply.github.com>
2026-05-18 09:30:31 +0530

    Refactor product API routes and improve functionality

--- week-3/api/productapi.js
+++ week-3/api/productapi.js
@@ -1,21 +1,51 @@
-import exp from'express'
-import { ProductModel } from '../models/productmodel.js'
-import { model } from 'mongoose'
-import { hash } from 'bcryptjs'
-export const ProductApp=exp.Router()
+import express from 'express'
+import {ProductModel} from '../models/ProductModel.js'
+// creating mini express application
 
-// add products
-ProductApp.post('/product',async(req,res)=>{
-    let newproduct=req.body
-    let newproductdoc=await new ProductModel(newproduct)
-    console.log(newproductdoc)
-    newproductdoc.save()
-    res.status(200).json({message:"user created",payload:newproductdoc})
+export const productApp = express.Router();
 
+
+productApp.post('/products',async(req,res)=>{
+    // getting the new product from the req body
+    let newProduct = req.body;
+    let newProductDoc = new ProductModel(newProduct);
+    await newProductDoc.save();
+    res.status(201).json({message:"product saved Successfully",payload:newProductDoc});
 })
 
-// get all products
-ProductApp.get('/product',async(req,res)=>{
-    let products=await ProductModel.find()
-    res.status(200).json({message:"products are :",payload:products})
+// get request to get all the products
+productApp.get('/products',async(req,res)=>{
+    let productList = await ProductModel.find();
+    res.status(200).json({message:"products",payload:productList})
+})
+
+// get with id
+productApp.get('/products/:id',async(req,res)=>{
+    let givenId = req.params.id;
+    let findProduct = await ProductModel.findById(givenId);
+    res.status(200).json({message:"product by id",payload:findProduct})
+})
+
+// update with id
+
+productApp.put('/products/:id',async(req,res)=>{
+    // extract id
+    let givenId = req.params.id;
+    let newProduct = req.body;
+    let latestObj = await ProductModel.findByIdAndUpdate(givenId,{$set:{...newProduct}},{new:true})
+    res.status(200).json({message:"updated the product",payload:latestObj})
+})
+
+// getting the product using pid
+productApp.get('/products/pid/:id',async(req,res)=>{
+    let givenPID = req.params.id;
+    console.log(givenPID)
+    // let findObj = ProductModel.findById(givenPID);
+    // console.log(findObj);
+    // the above one is not working 
+    // now let's try with our traditional approach
+    let allProducts = await ProductModel.find();
+    // console.log(allProducts)
+    let findProduct =  allProducts.filter((obj)=>obj.pid===givenPID);
+    console.log(findProduct);
 })
